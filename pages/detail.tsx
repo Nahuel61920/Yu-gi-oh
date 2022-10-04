8ef2ac7612928db42291b07223555f02c505a6f9
Nahuel61920 <estebancarrizo619@gmail.com>
2022-10-04 03:44:09 -0300

    Detail

--- pages/detail.tsx
+++ pages/detail.tsx
@@ -6,6 +6,18 @@ import deta from "../styles/Detail.module.css";
 
 const Detail: NextPage = () => {
     const { details } = useSelector((state: any) => state.card);
+
+    var star: any = [];
+
+    if (details.level) {
+        for (let i = 0; i < details.level; i++) {
+            star = [...star, <span key={i} className={deta.star}>&#9733;</span>];
+        }
+    } else {
+        null
+    }
+
+    console.log(star);
     return (
         <div className={deta.container}>
             <Head>
@@ -13,37 +25,61 @@ const Detail: NextPage = () => {
                 <meta name="description" content="Detail card" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <h2>{details.name}</h2>
-            <div className={deta.card}>
-                <img src={details.img} alt={details.name} />
+            {
+                details.name ? (
+                    <div className={deta.card}>
+                        <img src={details.img} alt={details.name} />
 
-                <div>
-                    {details.attribute ? (
-                        <div className={deta.containerAttribute}>
-                            <div className={"prev " + details.attribute}></div>
-                            <div>
-                                <span className={deta.attributes}>
-                                    {details.attribute.toLowerCase()} -{" "}
-                                </span>
-                                <span className={deta.attributes}>{details.race}</span>
-                            </div>
+                        <div className={deta.cardInfo}>
+                            <h2>{details.name}</h2>
+                            {details.attribute ? (
+                                <div className={deta.containerAttribute}>
+                                    <div className={"prev " + details.attribute}></div>
+                                    <div>
+                                        <span className={deta.attributes}>
+                                            {details.attribute.toLowerCase()} -{" "}
+                                        </span>
+                                        <span className={deta.attributes}>{details.race}</span>
+                                    </div>
+                                </div>
+                            ) : (
+                                <div className={deta.containerAttribute}>
+                                    <div
+                                        className={"prev " + details.type.split(" ").join("-")}
+                                    ></div>
+                                    <span className={deta.attributes}>
+                                        {details.type === "Spell Card"
+                                            ? details.race.split("Spell").join("")
+                                            : details.race.split("Trap").join("")}
+                                    </span>
+                                </div>
+                            )}
+                            <h3>[ {details.type.split(" ").join(" / ")} ]</h3>
+                            <p>{details.description}</p>
+                            {
+                                details.def || details.atk ? (
+                                    <div className={deta.containerAttribute}>
+                                        <div>
+                                            <span className={deta.attributes}>
+                                                ATK: {details.atk}
+                                            </span>
+                                            {" / "}
+                                            <span className={deta.attributes}>
+                                                DEF: {details.def}
+                                            </span>
+                                        </div>
+                                        <div className={deta.level}>
+                                            <span className={deta.attributes}>
+                                                Level: {star}
+                                            </span>
+                                        </div>
+                                    </div>
+                                ) : null
+                            }
                         </div>
-                    ) : (
-                        <div className={deta.containerAttribute}>
-                            <div
-                                className={"prev " + details.type.split(" ").join("-")}
-                            ></div>
-                            <span className={deta.attributes}>
-                                {details.type === "Spell Card"
-                                    ? details.race.split("Spell").join("")
-                                    : details.race.split("Trap").join("")}
-                            </span>
-                        </div>
-                    )}
-                    <h3>[ {details.type.split(" ").join(" / ")} ]</h3>
-                    <p>{details.description}</p>
-                </div>
-            </div>
+                    </div>
+                ) : null
+            }
         </div>
     );
 };
